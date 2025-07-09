91833116b5b835d7595791116925d0f36726afa4
Nadeem Ansari <94752890+Nadeem4380@users.noreply.github.com>
2025-07-09 08:31:07 +0000

    1st code done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "nadeem@gmail.com"
+var accountPassword = "12345"
+accountCity = " Ranchi"
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
