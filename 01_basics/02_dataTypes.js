c6adf8f0f0e535415c01d88f3215c6d3dea8d3bf
Nadeem Ansari <94752890+Nadeem4380@users.noreply.github.com>
2025-07-13 12:22:30 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) we are using nodejs not the browser
+console.log(3+3)
+console.log("Nadeem")
+
+let name = "Nadeem"
+let age = 25
+let isLoggedIn = false
+let state;
+//datatypes:
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
