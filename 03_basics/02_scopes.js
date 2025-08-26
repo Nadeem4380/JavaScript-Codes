389a2d3c7e39ae959a8f488cdfdd6d237ac42095
Nadeem Ansari <94752890+Nadeem4380@users.noreply.github.com>
2025-08-26 05:36:25 +0000

    scope and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,7 +5,7 @@ let a=300;
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
 // for(let i=0; i < Array.length; i++){
@@ -13,6 +13,46 @@ if(true){
 // }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "nadeem"
+
+    function two(){
+        const webstie = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+// one()
+
+if(true){
+    const username = "nadeem"
+    if(username === "nadeem"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+addone(5)
+function addone(num){
+    return num+1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
